166ed877b6b3a1dde37930ce7cfa108451854f5c
Otto <giotto007@gmail.com>
2018-06-08 11:37:25 +0100

    time to get ready added to alarm service

---
+++ Backend/WakeItHappn.Backend/WakeItHappen.Service/AlarmService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace WakeItHappen.Service
+{
+    public class AlarmService : IAlarm
+    {
+        private const int minutesToGetReady = 60;
+        
+        private readonly IRoutePlanner _routePlanner;
+        
+        public AlarmService(IRoutePlanner routePlanner)
+        {
+            _routePlanner = routePlanner;
+        }
+        
+        public AlarmRequest CreateAlarm(AlarmData alarmData)
+        {
+            var response = _routePlanner.GetTravelTime(new TravelTimeRequest
+            {
+                End = alarmData.To,
+                Start = alarmData.From,
+                Time = alarmData.ArrivalTime
+            });
+
+            return new AlarmRequest
+            {
+                AlarmId = Guid.NewGuid(),
+                AlarmTime = alarmData.ArrivalTime.AddMinutes(-response.TravelTimeMinutes-minutesToGetReady)
+            };
+        }
+    }
+}
---
+++ Backend/WakeItHappn.Backend/WakeItHappen.Service/CityMapperClient.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace WakeItHappen.Service
+{
+    public class CityMapperClient : IRoutePlanner
+    {
+        public TravelRouteResponse GetTravelTime(TravelTimeRequest request)
+        {
+            HttpClient client = new HttpClient
+            {
+                BaseAddress = new Uri("https://developer.citymapper.com/api/1/traveltime/")
+            };
+
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            string urlParameters = new CityMapperRequestBuilder().Build(new CityMapperRequest
+            {
+                Start = request.Start,
+                End = request.End,
+                Time = request.Time
+            });
+            
+            HttpResponseMessage response = client.GetAsync(urlParameters).Result;  // Blocking call!
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse the response body. Blocking!
+
+                var byteArrayMessage = response.Content.ReadAsByteArrayAsync();
+                var stringMessage = Encoding.ASCII.GetString(byteArrayMessage.Result);
+                
+                dynamic jsonMessage = Newtonsoft.Json.JsonConvert.DeserializeObject(stringMessage);
+                
+                string travel_time_minutes = jsonMessage.travel_time_minutes;
+
+
+                return new TravelRouteResponse
+                {
+                    TravelTimeMinutes = int.Parse(travel_time_minutes),
+                    TravelStatus = TravelStatus.Unknown,
+                    Details = new TravelDetails()
+                };
+
+            }
+
+            return new TravelRouteResponse();
+        }
+    }
+}
